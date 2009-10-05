66fe3449dcb4714ed1590547872e667244100b84
sylvain.pasche <sylvain.pasche@65eee992-c9ea-11dd-8829-37334faa441c>
2009-10-05 16:09:27 +0000

    converted to use unittest

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,5 +1,6 @@
 import httplib
 import os
+import unittest
 import urllib
 
 from w3testrunner.webapp import WebApp
@@ -19,112 +20,116 @@ class MockRunner(object):
             nouacheck = False
         self.options = MockOptions()
 
+class TestWebApp(unittest.TestCase):
+
+    def testRoot(self):
+        conn = httplib.HTTPConnection("localhost", 8888)
+        conn.request("GET", "/")
+        response = conn.getresponse()
+        self.assertEqual(response.status, 200)
+        self.assertEqual(response.reason, "OK")
+        self.assertEqual(response.getheader("content-type"), "text/html")
+        self.assertNotEqual(response.getheader("date"), None)
+        self.assertNotEqual(response.getheader("server"), None)
+        content = response.fp.read()
+        self.assertTrue("W3TestRunner" in content)
+
+    def testCGI(self):
+        conn = httplib.HTTPConnection("localhost", 8888)
+        conn.request("GET", "/hello_cgi.py")
+        response = conn.getresponse()
+        self.assertEqual(response.status, 404)
+        self.assertEqual(response.reason, "Not found")
+        self.assertEqual(response.getheader("content-type"), "text/plain")
+
+        webapp.enable_localtests(webapp_data_dir)
+
+        conn.request("GET", "/hello_cgi.py")
+        response = conn.getresponse()
+        self.assertEqual(response.status, 200)
+        self.assertEqual(response.reason, "OK")
+        self.assertEqual(response.getheader("content-type"), "text/plain")
+        content = response.fp.read()
+        self.assertEqual(content, "Test content here\n")
+
+        conn.request("GET", "/status_cgi.py")
+        response = conn.getresponse()
+        self.assertEqual(response.status, 500)
+        self.assertEqual(response.reason, "Doh, Server Error")
+        self.assertEqual(response.getheader("content-type"), "text/plain")
+        # the status_cgi.py^headers^ file should not be parsed when executing
+        # CGI.
+        self.assertEqual(response.getheader("X-Should-Be-Ignored"), None)
+        content = response.fp.read()
+        self.assertEqual(content, "Test content here\n")
+
+        conn.request("GET", "/webob_cgi.py")
+        response = conn.getresponse()
+        self.assertEqual(response.status, 200)
+        self.assertEqual(response.reason, "OK")
+        self.assertEqual(response.getheader("content-type"), "text/plain")
+        self.assertEqual(response.getheader("X-Some-Header"), "Foo Value")
+        content = response.fp.read()
+        self.assertEqual(content, "get_param:  post_param: ")
+
+        conn.request("GET", "/webob_cgi.py?get_param=A%20param")
+        response = conn.getresponse()
+        self.assertEqual(response.status, 200)
+        self.assertEqual(response.reason, "OK")
+        self.assertEqual(response.getheader("content-type"), "text/plain")
+        self.assertEqual(response.getheader("X-Some-Header"), "Foo Value")
+        content = response.fp.read()
+        self.assertEqual(content, "get_param: A param post_param: ")
+
+        params = urllib.urlencode({'post_param': 'A posted param'})
+        headers = {"Content-type": "application/x-www-form-urlencoded",
+                   "Accept": "text/plain"}
+        conn.request("POST", "/webob_cgi.py", params, headers)
+        response = conn.getresponse()
+        self.assertEqual(response.status, 200)
+        self.assertEqual(response.reason, "OK")
+        self.assertEqual(response.getheader("content-type"), "text/plain")
+        self.assertEqual(response.getheader("X-Some-Header"), "Foo Value")
+        content = response.fp.read()
+        self.assertEqual(content, "get_param:  post_param: A posted param")
+
+        webapp.disable_localtests()
+
+        conn.request("GET", "/hello_cgi.py")
+        response = conn.getresponse()
+        self.assertEqual(response.status, 404)
+        self.assertEqual(response.reason, "Not found")
+        self.assertEqual(response.getheader("content-type"), "text/plain")
+
+    def testHeaders(self):
+        conn = httplib.HTTPConnection("localhost", 8888)
+        webapp.enable_localtests(webapp_data_dir)
+
+        conn.request("GET", "/foo.xml")
+        response = conn.getresponse()
+        self.assertEqual(response.status, 200)
+        self.assertEqual(response.reason, "OK")
+        self.assertEqual(response.getheader("content-type"), "text/xml")
+        self.assertEqual(response.getheader("X-Some-Header"), "Foo Value")
+        content = response.fp.read()
+        self.assertEqual(content,
+                         """<?xml version="1.0" encoding="UTF-8"?><foo/>\n""")
+
+        conn.request("GET", "/foo.oga")
+        response = conn.getresponse()
+        self.assertEqual(response.status, 404)
+        self.assertEqual(response.reason, "Sorry, can't find it")
+        self.assertEqual(response.getheader("content-type"), "audio/ogg")
+        self.assertEqual(response.getheader("X-Some-Header2"), "Foo Value 2")
+        content = response.fp.read()
+        self.assertEqual(content, "Test content here\n")
+
+        webapp.disable_localtests()
+
 def setup():
     global webapp
     mock_runner = MockRunner()
     webapp = WebApp(mock_runner)
-
-def test_root():
-    conn = httplib.HTTPConnection("localhost", 8888)
-    conn.request("GET", "/")
-    response = conn.getresponse()
-    assert response.status == 200
-    assert response.reason == "OK"
-    assert response.getheader("content-type") == "text/html"
-    assert response.getheader("date") != None
-    assert response.getheader("server") != None
-    content = response.fp.read()
-    assert "W3TestRunner" in content
-
-def test_cgi():
-    conn = httplib.HTTPConnection("localhost", 8888)
-    conn.request("GET", "/hello_cgi.py")
-    response = conn.getresponse()
-    assert response.status == 404
-    assert response.reason == "Not found"
-    assert response.getheader("content-type") == "text/plain"
-
-    webapp.enable_localtests(webapp_data_dir)
-
-    conn.request("GET", "/hello_cgi.py")
-    response = conn.getresponse()
-    assert response.status == 200
-    assert response.reason == "OK"
-    assert response.getheader("content-type") == "text/plain"
-    content = response.fp.read()
-    assert content == "Test content here\n"
-
-    conn.request("GET", "/status_cgi.py")
-    response = conn.getresponse()
-    assert response.status == 500
-    assert response.reason == "Doh, Server Error"
-    assert response.getheader("content-type") == "text/plain"
-    # the status_cgi.py^headers^ file should not be parsed when executing CGI.
-    assert response.getheader("X-Should-Be-Ignored") == None
-    content = response.fp.read()
-    assert content == "Test content here\n"
-
-    conn.request("GET", "/webob_cgi.py")
-    response = conn.getresponse()
-    assert response.status == 200
-    assert response.reason == "OK"
-    assert response.getheader("content-type") == "text/plain"
-    assert response.getheader("X-Some-Header") == "Foo Value"
-    content = response.fp.read()
-    assert content == "get_param:  post_param: "
-
-    conn.request("GET", "/webob_cgi.py?get_param=A%20param")
-    response = conn.getresponse()
-    assert response.status == 200
-    assert response.reason == "OK"
-    assert response.getheader("content-type") == "text/plain"
-    assert response.getheader("X-Some-Header") == "Foo Value"
-    content = response.fp.read()
-    assert content == "get_param: A param post_param: "
-
-    params = urllib.urlencode({'post_param': 'A posted param'})
-    headers = {"Content-type": "application/x-www-form-urlencoded",
-               "Accept": "text/plain"}
-    conn.request("POST", "/webob_cgi.py", params, headers)
-    response = conn.getresponse()
-    assert response.status == 200
-    assert response.reason == "OK"
-    assert response.getheader("content-type") == "text/plain"
-    assert response.getheader("X-Some-Header") == "Foo Value"
-    content = response.fp.read()
-    assert content == "get_param:  post_param: A posted param"
-
-    webapp.disable_localtests()
-
-    conn.request("GET", "/hello_cgi.py")
-    response = conn.getresponse()
-    assert response.status == 404
-    assert response.reason == "Not found"
-    assert response.getheader("content-type") == "text/plain"
-
-def test_headers():
-    conn = httplib.HTTPConnection("localhost", 8888)
-    webapp.enable_localtests(webapp_data_dir)
-
-    conn.request("GET", "/foo.xml")
-    response = conn.getresponse()
-    assert response.status == 200
-    assert response.reason == "OK"
-    assert response.getheader("content-type") == "text/xml"
-    assert response.getheader("X-Some-Header") == "Foo Value"
-    content = response.fp.read()
-    assert content == """<?xml version="1.0" encoding="UTF-8"?><foo/>\n"""
-
-    conn.request("GET", "/foo.oga")
-    response = conn.getresponse()
-    assert response.status == 404
-    assert response.reason == "Sorry, can't find it"
-    assert response.getheader("content-type") == "audio/ogg"
-    assert response.getheader("X-Some-Header2") == "Foo Value 2"
-    content = response.fp.read()
-    assert content == "Test content here\n"
-
-    webapp.disable_localtests()
 
 def teardown():
     webapp.running = False
